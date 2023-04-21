038a1b2690c5ecc5018e1eeaf205243857ebb960
dymafr <contact@dyma.fr>
2023-04-21 17:15:38 +0200

    maj 6.2.9 wip

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -10,7 +10,7 @@ use App\Form\QuestionType;
 use App\Repository\QuestionRepository;
 use App\Repository\VoteRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
